5dc5e6fe8819f8b9fd681be2607eb67426c49b5a
hypernagox <128601833+hypernagox@users.noreply.github.com>
2024-05-22 23:44:12 +0900

    브로드캐스터

---
+++ Server/ServerCore/MoveBroadcaster.cpp
@@ -0,0 +1,113 @@
+#include "ServerCorePch.h"
+#include "MoveBroadcaster.h"
+#include "PacketSession.h"
+
+
+namespace ServerCore
+{
+	MoveBroadcaster::MoveBroadcaster()
+	{
+	}
+
+	MoveBroadcaster::~MoveBroadcaster()
+	{
+	}
+
+	const int MoveBroadcaster::BroadcastMove(
+		const S_ptr<SendBuffer>& in_pkt,
+		const S_ptr<SendBuffer>& out_pkt,
+		const S_ptr<SendBuffer>& move_pkt,
+		const S_ptr<IocpEntity>& thisSession_,
+		const Vector<SessionManageable*>* const sectors
+	)noexcept
+	{
+		if (IDLE != m_work_flag.exchange(WORK, std::memory_order_relaxed))
+			return NONE;
+		int sector_state = 0;
+		
+		const uint16 obj_type = thisSession_->GetObjectType();
+		const auto thisSession = thisSession_->IsSession();
+		if (thisSession)
+		{
+			if (false == thisSession->IsConnectedAtomic())
+				return NONE;
+		}
+		const auto cache_obj_ptr = thisSession_.get();
+		const bool bIsNPC = 0 != obj_type;
+
+		thread_local HashSet<S_ptr<IocpEntity>> new_view_list;
+		new_view_list.clear();
+		for (const auto sector : *sectors)
+		{
+			//sector->GetSRWLock().lock_shared();
+			//for (const auto pSession : sector->GetObjectList())
+			//{
+			//	const bool bFlag = static_cast<const bool>(pSession->IsSession());
+			//	sector_state |= (bFlag + 1);
+			//	if (bIsNPC && !bFlag)
+			//		continue;
+			//	if (auto pValid = pSession->GetSharedThis())
+			//	{
+			//		if (g_huristic(cache_obj_ptr, pSession))
+			//		{
+			//			new_view_list.emplace(std::move(pValid));
+			//		}
+			//	}
+			//}
+			//sector->GetSRWLock().unlock_shared();
+		}
+		new_view_list.erase(thisSession_);
+
+		for (const auto& pEntity : new_view_list)
+		{
+			const auto pSession = pEntity->IsSession();
+			if (!m_viewList.contains(pEntity))
+			{
+				
+				if (pSession)
+					pSession->SendAsync(in_pkt);
+
+				if (thisSession)
+					thisSession->SendAsync(g_create_in_pkt(pEntity));
+				
+				m_viewList.emplace(pEntity);
+			}
+			else
+			{
+				if (thisSession)
+					thisSession->SendAsync(move_pkt);
+				if (pSession)
+					pSession->SendAsync(move_pkt);
+			}
+		}
+
+		const auto e_iter = new_view_list.cend();
+		
+		for (auto iter = m_viewList.cbegin(); iter != m_viewList.cend();)
+		{
+			const auto& pEntity = *iter;
+			const auto target = new_view_list.find(pEntity);
+			const auto pSession = pEntity->IsSession();
+			if (e_iter == target)
+			{
+				if (pSession)
+					pSession->SendAsync(out_pkt);
+				if (thisSession)
+					thisSession->SendAsync(g_create_out_pkt(pEntity));
+				iter = m_viewList.erase(iter);
+			}
+			else
+			{
+				++iter;
+			}
+		}
+		
+		if (WORK != m_work_flag.exchange(IDLE, std::memory_order_release))
+		{
+			m_viewList.clear();
+			return STOP;
+		}
+			
+		return 3 ^ sector_state;
+	}
+}
---
+++ Server/ServerCore/MoveBroadcaster.h
@@ -0,0 +1,70 @@
+#pragma once
+
+namespace ServerCore
+{
+	class IocpEntity;
+	class Session;
+	class SendBuffer;
+	class PacketSession;
+
+	enum SECTOR_STATE
+	{
+		NOT_EMPTY = 0,
+		NPC_EMPTY = 1 << 0,
+		USER_EMPTY = 1 << 1,
+		EMPTY = 3,
+		STOP = 1 << 2,
+
+		IDLE,
+		WORK,
+
+		NONE,
+	};
+
+	class MoveBroadcaster
+	{
+	public:
+		MoveBroadcaster();
+		~MoveBroadcaster();
+		using HuristicFunc = bool(*)(const IocpEntity* const, const IocpEntity* const);
+		using PacketFunc = S_ptr<SendBuffer>(*)(const S_ptr<IocpEntity>&);
+	public:
+
+		const int BroadcastMove(
+			const S_ptr<SendBuffer>& in_pkt,
+			const S_ptr<SendBuffer>& out_pkt,
+			const S_ptr<SendBuffer>& move_pkt,
+			const S_ptr<IocpEntity>& thisSession,
+			const Vector<SessionManageable*>* const sectors
+		)noexcept;
+		
+		void ReleaseViewList()noexcept
+		{
+			if (IDLE == m_work_flag.exchange(STOP, std::memory_order_relaxed))
+			{
+				std::atomic_thread_fence(std::memory_order_acquire);
+				m_viewList.clear();
+			}
+		}
+	public:
+		static void RegisterHuristicFunc(const HuristicFunc fp_)noexcept {
+			if (g_huristic)return;
+			g_huristic = (fp_);
+		}
+		static void RegisterInPacketFunc(const PacketFunc fp_)noexcept {
+			if (g_create_in_pkt)return;
+			g_create_in_pkt = (fp_);
+		}
+		static void RegisterOutPacketFunc(const PacketFunc fp_)noexcept {
+			if (g_create_out_pkt)return;
+			g_create_out_pkt = (fp_);
+		}
+	private:
+		std::atomic<SECTOR_STATE> m_work_flag = IDLE; 
+		HashSet<S_ptr<IocpEntity>> m_viewList;
+	private:
+		static inline HuristicFunc g_huristic = {};
+		static inline PacketFunc g_create_in_pkt = {};
+		static inline PacketFunc g_create_out_pkt = {};
+	};
+}
