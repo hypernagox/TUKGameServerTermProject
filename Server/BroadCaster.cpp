608eb2c76c96bd3d97aed2a1a06e212d7d21a53a
hypernagox <128601833+hypernagox@users.noreply.github.com>
2024-06-16 22:53:31 +0900

    브로드캐스터 다시

--- Server/BroadCaster.cpp
+++ Server/BroadCaster.cpp
@@ -4,24 +4,28 @@
 #include "SessionManageable.h"
 #include "c2s_PacketHandler.h"
 #include "PhysicsComponent.h"
+#include "TRWorldMgr.h"
+#include "TRWorldChunk.h"
 
 void MoveBroadCaster::PostUpdate(const float) noexcept
 {
 	const auto obj = GetOwner();
-	const auto pRigid = obj->GetComp("RIGIDBODY")->Cast<RigidBody>();
+	const auto pRigid = obj->GetComp<RigidBody>();
 	Protocol::s2c_MOVE pkt;
 
 	pkt.set_obj_id(obj->GetObjID());
 	*pkt.mutable_obj_pos() = ::ToProtoVec2(obj->GetPos());
 	*pkt.mutable_wiil_pos() = ToProtoVec2(obj->GetWillPos());
-	*pkt.mutable_vel() = ::ToProtoVec2(pRigid->GetVelocity());
+	//*pkt.mutable_vel() = ::ToProtoVec2(pRigid->GetVelocity());
 	pkt.set_time_stamp(ServerCore::GetTimeStampMilliseconds());
-	*pkt.mutable_accel()= ::ToProtoVec2(pRigid->GetAccel());
+	//*pkt.mutable_accel()= ::ToProtoVec2(pRigid->GetAccel());
 	pkt.set_state((Protocol::PLAYER_STATE)obj->GetState());
 
 	if (obj->GetObjectGroup() == GROUP_TYPE::DROP_ITEM)
 	{
 		//std::cout << obj->GetPos().x << ", " << obj->GetPos().y << std::endl;
+		//m_pCurRoom << pkt;
 	}
-	m_pCurRoom << pkt;
+	TRMgr(TRWorldMgr)->GetStartWorld()->BroadCastToWorld(ServerCore::c2s_PacketHandler::MakeSendBuffer(pkt));
+	//m_pCurRoom << pkt;
 }
